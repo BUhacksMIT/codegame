a3b15c339c07f50e7a36dd6ec57230cce6133639
lasoren <lasoren@me.com>
2013-10-05 15:37:13 -0400

    fileWriter prints opcode to each log line first

--- fileWriter.py
+++ fileWriter.py
@@ -1,3 +1,5 @@
+import opCodes
+
 class fileWriter:
     
     logFile = open('log.txt','w')
@@ -6,20 +8,20 @@ class fileWriter:
         self.logFile.close()
 
     #Each line represents a command    
-    def writeLog(self,playerID,opCode,loc, newLoc = (-1,-1)):
+    def writeLog(self,playerID,opCode,loc, newLoc = (-1,-1), **kwargs):
         #create ship
-        if opCode == 1:
+        if opCode == opCodes.instantiate:
             # "playerID opCode (x,y)" where (x,y) is a location on the grid
-            print(playerID,1,loc,file = self.logFile,end="\r\n")
+            print(opCodes.instantiate,playerID,loc,file = self.logFile,end="\r\n")
         #Move ship from loc to newLoc
-        elif opCode == 2:
+        elif opCode == opCodes.move:
             # "playerID opcode (x,y) (a,b)" where (x,y) and (a,b) are locations on the grid
-            print(playerID,2,loc,newLoc,file = self.logFile,end="\r\n")
+            print(opCodes.move,playerID,loc,newLoc,file = self.logFile,end="\r\n")
         #Fire from loc to newLoc
-        elif opCode == 3:
+        elif opCode == opCodes.fire:
             # "playerID opcode (x,y) (a,b)" where (x,y) and (a,b) are locations on the grid
-            print(playerID,3,loc,newLoc,file = self.logFile,end="\r\n")
+            print(opCodes.fire,playerID,loc,newLoc,file = self.logFile,end="\r\n")
         #Remove a ship from the grid
-        elif opCode == 4:
+        elif opCode == opCodes.kill:
             # "playerID opCode (x,y)" where (x,y) is a location on the grid
-            print(playerID,4,loc,file = self.logFile,end="\r\n")
+            print(opCodes.kill,playerID,loc,file = self.logFile,end="\r\n")
--- logParse.cs
+++ logParse.cs
@@ -1,10 +1,30 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
 using System.IO.FileInfo;
  
 fileInfo= new FileInfo(fileName);
 
+string log;
 string myPath = fileInfo.DirectoryName;;
-if (!System.IO.Directory.Exists(myPath))
-    System.IO.Directory.CreateDirectory(myPath+@"\log");
+if (System.IO.Directory.Exists(myPath+@"\log")) {
+    string log = File.ReadAllText(myPath+@"\log"+@"\log.txt");
+    char newLine = "\r\n";
+    string[] commands = log.Split(newLine);
+    for (int i = 0; i < commands.Length; i++) {
+    }
+}
 
 
