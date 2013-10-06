9a0f680d76fffc918a2398491cd2ac5e61b9bb52
John <sniper231@gmail.com>
2013-10-05 23:05:57 -0400

    more communications, client cleanup, sample AI

--- client.py
+++ client.py
@@ -1,8 +1,17 @@
 import socket
 import sys
 import time
-import logging
 import pickle
+
+class resultcodes():
+    success = 1
+    hit = 2
+    fail = -1
+    toomanyships = -2
+    alreadyoccupied = -3
+    outofbounds = -4
+    outofrange = -5
+    miss = -6
 
 class fileOpcodes():
     instantiate = 1
@@ -44,86 +53,63 @@ class Ship():
         self.player = player
         self.health = 100
 
-def SendCommand(opcode, *args):
-    message = str(opcode) + ","
-    for arg in args:
-        message += str(arg) + ","
-    print("sending command: " + message)
-    s.send(bytes(str(message), "UTF-8"))
-    response = s.recv(4096)
-    print(response)
-    return response
+class Client():
 
-def Initialize_Ship(x, y):
-    print("init ship")
-    return pickle.loads(SendCommand(1, x, y))
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.game_started = False
 
-def GetPlayerCoords():
-    response = SendCommand(Opcodes.get_player_coords)
-    print("res:",response)
-    response = pickle.loads(response)
-    print("res2",response)
-    return response
+    def _SendCommand(opcode, *args):
+        message = str(opcode) + ","
+        for arg in args:
+            message += str(arg) + ","
+        print("sending command: " + message)
+        s.send(bytes(str(message), "UTF-8"))
+        response = self.s.recv(4096)
+        print(response)
+        return response
 
-def Move(shipid, direction):
-    return pickle.loads(SendCommand(Opcodes.move, shipid, direction))
+    def InitializeShip(x, y):
+        print("init ship")
+        return pickle.loads(self._SendCommand(Opcodes.initialize_ship, x, y))
 
-def GetDelay():
-    return pickle.loads(SendCommand(Opcodes.get_delay))
-def Fire(shipid, to_x, to_y):
-    return pickle.loads(SendCommand(Opcodes.fire, shipid, to_x, to_y))
+    def GetPlayerCoords():
+        response = self._SendCommand(Opcodes.get_player_coords)
+        print("res:",response)
+        response = pickle.loads(response)
+        print("res2",response)
+        return response
 
-def GetMyAliveShips():
-    return pickle.loads(SendCommand(Opcodes.get_my_alive_ships))
+    def Move(shipid, direction):
+        return pickle.loads(self._SendCommand(Opcodes.move, shipid, direction))
 
-def GetGameStatus():
-    return pickle.loads(SendCommand(Opcodes.get_game_status))
+    def GetMyDelay():
+        return pickle.loads(self._SendCommand(Opcodes.get_delay))
 
-ip, port = "localhost", 1338
+    def Fire(shipid, to_x, to_y):
+        return pickle.loads(self._SendCommand(Opcodes.fire, shipid, to_x, to_y))
 
+    def GetMyAliveShips():
+        return pickle.loads(self._SendCommand(Opcodes.get_my_alive_ships))
 
-logger = logging.getLogger('client')
-logger.info('client on %s:%s', ip, port)
+    def _GetGameStatus():
+        return pickle.loads(self._SendCommand(Opcodes.get_game_status))
 
-# Connect to the server
-logger.debug('creating socket')
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-logger.debug('connecting to server')
-s.connect((ip, port))
-
-# Send the data
-message = 'Hello, world yo 4'
-logger.debug('sending data: "%s"', message)
-
-SendCommand(Opcodes.choose_lang, langs.Python) # 1 is for Python
-game_started = False
-while (game_started == False):
-    rescode, resval = GetGameStatus()
-    if (resval == True):
-        game_started = True
-        print("Game started!")
-rescode, resval = Initialize_Ship(5, 5)
-print(rescode)
-print(resval)
-shiptomove=resval
-while (True):
-    rescode, resval = GetDelay()
-    print(resval)
-    if (resval > 0):
-        print("I can't move for another ", str(resval), " ticks!")
-    else:
-        rescode, resval = Fire(shiptomove, 15, 15)
-        print(rescode)
-        print(resval)
-    time.sleep(1);
-
-# Receive a response
-logger.debug('waiting for response')
-response = s.recv(len_sent)
-logger.debug('response from server: "%s"', response)
-
-# Clean up
-logger.debug('closing socket')
-s.close()
-logger.debug('done')
-
+    def ConnectToGame(host, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.ip, self.port))
+        self._SendCommand(Opcodes.choose_lang, langs.Python)
+        while (self.game_started == False):
+            rescode, resval = self._GetGameStatus()
+            if (resval[0] == True):
+                self.board_width = int(resval[1])
+                self.board_height = int(resval[2])
+                self.max_range = int(resval[3])
+                self.max_ships = int(resval[4])
+                self.game_started = True
+                print("Game started!")
+        return True
+    
+    def __del__(self):
+        self.s.close()
