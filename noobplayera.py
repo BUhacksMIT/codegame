f94529568ab84e0e08df5476b7c96263e27df163
lasoren <lasoren@me.com>
2013-10-06 07:31:04 -0400

    Player code updated

--- noobplayera.py
+++ noobplayera.py
@@ -1,8 +1,10 @@
 from client import Client
 from client import resultcodes
 from client import Directions
+from client import Ship
 import random
 import time
+import math
 
 random.seed()
 
@@ -10,39 +12,49 @@ gameclient = Client("localhost", 1339)
 
 gameclient.ConnectToGame()
 
-myships = []
-mycoords = []
+mycoords = {}
 
-offset = 3
 for i in range(gameclient.max_ships):
-    y = random.choice([2, 3, 4, 5, 6])
-    rescode, resval = gameclient.InitializeShip(3+i-offset, y)
-    if (rescode == resultcodes.success):
-        mycoords.append((3+i-offset, y))
-        myships.append(resval)
-        print ("ship success")
-    else:
-        print ("ship fail. try again...")
-        offset += 1
-
-k=0
-i=0
+	y = random.choice([1,2,3,4,5])
+	rescode, resval = gameclient.InitializeShip(i*2,y)
+	if (rescode == resultcodes.success):
+		mycoords[resval] = (i*2,y)
+		print("Ship successfully created")
+k = 0
+i = 0	
 while (True):
-    print("getting delay")
-    rescode, resval = gameclient.GetMyDelay()
-    print("delay is ", resval, " with code ", rescode)
-    if (resval > 0):
-        print("I can't move for another ", str(resval), " ticks!")
-    else:
-        if (k % 2 == 0):
-            rescode, resval = gameclient.Fire(myships[i % len(myships)], mycoords[i % len(mycoords)][0]+2, mycoords[i % len(mycoords)][1]+4)
-            print("fire  ", i, " to ",  mycoords[i % len(mycoords)][0]+random.choice([-1, 1])*random.randint(1, 5), ",", mycoords[i % len(mycoords)][1]+random.choice([-1, 1])*random.randint(1, 5), " with result ", rescode, " and value ", resval)
-        else:
-            rescode, resval = gameclient.Move(myships[i % len(myships)], random.randint(1,8))
-            print("move  ", i, " to right with result ", rescode, " and value ", resval)
-    k = k + 1
-    i= (i+ 1)
-    time.sleep(0.1)
-
+	rescode, resval = gameclient.GetMyDelay()
+	print("delay is ", resval, " with code ", rescode)
+	if (resval > 0):
+		print("I can't move for another ", str(resval), " ticks!")
+		rescode, resval = gameclient.GetMyAliveShips()
+		for id in mycoords:
+			if id not in resval:
+				del mycoords[id]
+	else:
+		if (k % 2 == 0):
+			rescode, opShips = gameclient.GetPlayerCoords()
+			for ship in opShips:
+				print(ship.coords)
+		else:
+			#find closest ship
+			minDistance = 40
+			for ship in opShips:
+				for id in mycoords:
+					dist = math.sqrt((ship.x-mycoords[id][0])**2 + (ship.y-mycoords[id][1])**2)
+					if dist < minDistance and ship.alive:
+						minDistance = dist
+						shipID = id
+						fireX = ship.coords[0]
+						fireY = ship.coords[1]
+			print("MinDistance",minDistance,fireX,fireY)
+			if minDistance < 8:
+				rescode, resval = gameclient.Fire(shipID,fireX,fireY)
+				#print("Fire to {0}, {1}",String.Format(fireX,fireY)
+			else:
+				rescode, resval = gameclient.Move(shipID,1)
+		k = k + 1
+	time.sleep(0.1)
+					
 s.close()
 
